fe572cf4517c0d875970f27522678815d10438c9
JlwxdQueen <jlwxd@mail.ru>
2025-09-30 21:26:28 +0300

    update: config for pg linking

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,6 +4,11 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "todo_API"
     api_version: str = "1.0.0"
+    database_url: str = "postgresql+asyncpg://user:password@localhost:5432/todo_db"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 settings = Settings()
